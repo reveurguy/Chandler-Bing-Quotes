fb7d038e9bfbc7cf996ad28676afd5a3abf220d8
Priyanshu Singh <59417546+reveurguy@users.noreply.github.com>
2020-10-26 20:34:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var btn= document.querySelector("#btn");
 var quote= document.querySelector("#quote");
+var tweetbtn= document.querySelector("#twitterbtn");
+var fbbtn= document.querySelector("#facebookbtn");
 // var url= 'https://api.sheety.co/de1e3565f4fdbd0a2999a03f48817ce1/chandlerBing/chandlerBing';
 var url= 'https://sheet.best/api/sheets/d5afee0f-b41f-4e88-bf3f-3ec18bf2e1bb';
 // var url= 'https://sheet.best/api/sheets/9eb26f2c-b413-4dcb-a943-c3767e797f29';
@@ -25,4 +27,14 @@ btn.addEventListener("click",function () {
         })
     })
 });
+
+tweetbtn.addEventListener("click", function() {
+  tweetIt ();
+});
+
+function tweetIt () {
+  var phrase = quote.innerText;
+  var tweetUrl = "https://twitter.com/intent/tweet?&text=\"" + encodeURIComponent(phrase) + "\"  - Chandler Bing &hashtags=FRIENDS&hashtags=CHANDLERBING&url=https://reveurguy.github.io/Chandler-Bing-Quotes/"; 
+  window.open(tweetUrl);
+}
 
